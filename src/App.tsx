801ca02028c79259fb5bfbf5123fc5135e69dd73
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-07-14 12:58:44 +0900

    :heavy_plus_sign: Add - 중첩된 라우팅

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import {useState} from 'react';
+import Orders from '@src/pages/Orders';
+import Delivery from '@src/pages/Delivery';
+import SignIn from '@src/pages/SignIn';
+import SignUp from '@src/pages/SignUp';
+import Mypage from '@src/pages/Mypage';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+export type LoggedInParamList = {
+  Orders: undefined;
+  Settings: undefined;
+  Delivery: undefined;
+  Complete: {orderId: string};
+};
+
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Mypage"
+            component={Mypage}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
+
+export default App;
