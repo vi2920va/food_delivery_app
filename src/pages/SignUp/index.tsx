53285a9c533591c44e0f3ee4655adaae89d3f751
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-07-19 19:47:19 +0900

    :memo: Add - axios로 활용하여 서버 요청 보내기 기능

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -9,11 +9,17 @@ import {
   TextInputChangeEventData,
   Platform,
   Alert,
+  ActivityIndicator,
 } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import DismissKeyboardView from '@src/components/atoms/DismissKeyboardView';
+import axios, {AxiosError} from 'axios';
+import {RootStackParamList} from '@src/AppInner';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
-const SignUp = () => {
+const SignUp = ({navigation}: SignUpScreenProps) => {
+  const [loading, setLoading] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -48,16 +54,23 @@ const SignUp = () => {
     setChecked(!checked);
   }, [checked]);
 
-  const onSubmit = useCallback(() => {
+  const onSubmit = useCallback(async () => {
+    if (loading) {
+      return loading;
+    }
+
     if (!name || !name.trim()) {
       return Alert.alert('알림', '이름을 입력해주세요.');
     }
+
     if (!email || !email.trim()) {
       return Alert.alert('알림', '이메일을 입력해주세요.');
     }
+
     if (!password || !password.trim()) {
       return Alert.alert('알림', '비밀번호를 입력해주세요.');
     }
+
     if (
       !/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(
         email,
@@ -65,99 +78,122 @@ const SignUp = () => {
     ) {
       return Alert.alert('알림', '올바른 이메일 주소가 아닙니다.');
     }
+
     if (!/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@^!%*#?&]).{8,50}$/.test(password)) {
       return Alert.alert(
         '알림',
         '비밀번호는 영문,숫자,특수문자($@^!%*#?&)를 모두 포함하여 8자 이상 입력해야합니다.',
       );
     }
+
     if (!checked) {
       return Alert.alert('알림', '약관에 동의해주세요');
     }
-    Alert.alert('알림', '회원가입 되었습니다.');
-  }, [name, email, password, checked]);
+
+    try {
+      setLoading(true);
+      const response = await axios.post('/User', {
+        name,
+        email,
+        password,
+      });
+      setLoading(false);
+      Alert.alert('알림', '회원가입 되었습니다.');
+      console.log(response);
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      navigation.navigate('SignIn');
+      console.error();
+      if (errorResponse) {
+        Alert.alert('알림', errorResponse.data.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [name, email, password, checked, loading, navigation]);
   return (
     <DismissKeyboardView>
-      <View>
-        <View style={styles.inputWrapper}>
-          <Text style={styles.label}>이름</Text>
-          <TextInput
-            style={styles.textInput}
-            value={name}
-            placeholder="이름을 입력해주세요."
-            placeholderTextColor="#666"
-            onChange={event => onChangeName(event)}
-            onSubmitEditing={() => nameRef.current?.focus()}
-            textContentType="name"
-            returnKeyType="next"
-            clearButtonMode="while-editing"
-            ref={nameRef}
-            blurOnSubmit={false}
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이름</Text>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          placeholder="이름을 입력해주세요."
+          placeholderTextColor="#666"
+          onChange={event => onChangeName(event)}
+          onSubmitEditing={() => nameRef.current?.focus()}
+          textContentType="name"
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={nameRef}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          value={email}
+          placeholder="이메일을 입력해주세요."
+          placeholderTextColor="#666"
+          onChange={event => onChangeEmail(event)}
+          onSubmitEditing={() => emailRef.current?.focus()}
+          importantForAutofill="yes"
+          autoComplete="email"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          ref={emailRef}
+          clearButtonMode="while-editing"
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          value={password}
+          placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
+          onChange={event => onChangePassword(event)}
+          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
+          placeholderTextColor="#666"
+          importantForAutofill="yes"
+          autoComplete="password"
+          textContentType="password"
+          returnKeyType="send"
+          ref={passwordRef}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          blurOnSubmit={false}
+          clearButtonMode="while-editing"
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>약관</Text>
+        <View style={styles.checkWrapper}>
+          <CheckBox
+            style={styles.checkBox}
+            value={checked}
+            onChange={onChangeChecked}
           />
+          <Text style={styles.checkboxText}>약관에 동의하시겠습니까?</Text>
         </View>
-        <View style={styles.inputWrapper}>
-          <Text style={styles.label}>이메일</Text>
-          <TextInput
-            style={styles.textInput}
-            value={email}
-            placeholder="이메일을 입력해주세요."
-            placeholderTextColor="#666"
-            onChange={event => onChangeEmail(event)}
-            onSubmitEditing={() => emailRef.current?.focus()}
-            importantForAutofill="yes"
-            autoComplete="email"
-            textContentType="emailAddress"
-            keyboardType="email-address"
-            returnKeyType="next"
-            blurOnSubmit={false}
-            ref={emailRef}
-            clearButtonMode="while-editing"
-          />
-        </View>
-        <View style={styles.inputWrapper}>
-          <Text style={styles.label}>비밀번호</Text>
-          <TextInput
-            style={styles.textInput}
-            value={password}
-            placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
-            onChange={event => onChangePassword(event)}
-            keyboardType={
-              Platform.OS === 'android' ? 'default' : 'ascii-capable'
-            }
-            placeholderTextColor="#666"
-            importantForAutofill="yes"
-            autoComplete="password"
-            textContentType="password"
-            ref={passwordRef}
-            onSubmitEditing={() => passwordRef.current?.focus()}
-            blurOnSubmit={false}
-            clearButtonMode="while-editing"
-            secureTextEntry
-          />
-        </View>
-        <View style={styles.inputWrapper}>
-          <Text style={styles.label}>약관</Text>
-          <View style={styles.checkWrapper}>
-            <CheckBox
-              style={styles.checkBox}
-              value={checked}
-              onChange={onChangeChecked}
-            />
-            <Text style={styles.checkboxText}>약관에 동의하시겠습니까?</Text>
-          </View>
-        </View>
-        <View style={styles.buttonZone}>
-          <Pressable
-            style={
-              canGoNext
-                ? StyleSheet.compose(styles.button, styles.buttonActive)
-                : styles.button
-            }
-            disabled={!canGoNext}
-            onPress={onSubmit}>
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={
+            canGoNext
+              ? StyleSheet.compose(styles.button, styles.buttonActive)
+              : styles.button
+          }
+          disabled={!canGoNext || loading}
+          onPress={onSubmit}>
+          {loading ? (
+            <ActivityIndicator color="white" />
+          ) : (
             <Text style={styles.buttonText}>회원가입</Text>
-          </Pressable>
-        </View>
+          )}
+        </Pressable>
       </View>
     </DismissKeyboardView>
   );
